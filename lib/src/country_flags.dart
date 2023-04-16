64576c5492667b7857dec88ae5624d6b96cf6f8a
Arturo Grau <arturo@fluttered.io>
2023-04-16 19:58:36 +0200

    feat: add the hability to create flags from a language code

--- lib/src/country_flags.dart
+++ lib/src/country_flags.dart
@@ -2,21 +2,36 @@ import 'package:country_flags/country_flags.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-/// Country flag widget
-class CountryFlags extends StatelessWidget {
-  /// Creates a flag widget.
-  CountryFlags.flag(
+/// {@template country_flags}
+/// A widget that displays a country flag.
+/// {@endtemplate}
+class CountryFlag extends StatelessWidget {
+  /// Create an instance of [CountryFlag] based on a language.
+  ///
+  /// {@macro country_flags}
+  CountryFlag.fromLanguageCode(
+    String languageCode, {
+    super.key,
+    this.height,
+    this.width,
+    this.borderRadius,
+  }) : flagCode = FlagCode.fromLanguageCode(languageCode.toLowerCase());
+
+  /// Create an instance of [CountryFlag] based on a country code.
+  ///
+  /// {@macro country_flags}
+  CountryFlag.fromCountryCode(
     String countryCode, {
     super.key,
     this.height,
     this.width,
     this.borderRadius,
-  }) : countryCode = countryCode.toLowerCase();
+  }) : flagCode = FlagCode.fromCountryCode(countryCode.toUpperCase());
 
   /// The country ISO code of the flag to display.
   ///
   /// The list of country codes can be found here: https://www.iban.com/country-codes.
-  final String countryCode;
+  final String? flagCode;
 
   /// The height of the flag.
   final double? height;
@@ -34,10 +49,10 @@ class CountryFlags extends StatelessWidget {
       child: SizedBox(
         width: width,
         height: height,
-        child: flagsCountryCodes.contains(countryCode)
+        child: flagCode != null
             ? SvgPicture.asset(
-                'packages/country_flags/res/$countryCode.svg',
-                semanticsLabel: countryCode,
+                'packages/country_flags/res/$flagCode.svg',
+                semanticsLabel: flagCode,
               )
             : const ColoredBox(
                 color: Colors.white,
