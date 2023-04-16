eae3b2b7b676a96828f143770a7f63966edbb7f9
Arturo Grau <93156071+arturograu@users.noreply.github.com>
2023-04-16 21:57:59 +0200

    fix: github actions workflow syntax (#7)
    * fix: github actions workflow syntax
    
    * feat: upgrade very_good_analysis
    
    * feat: add upload to codecov github action
    
    * feat: customize github actions
    
    * fix: fix code coverage path in github actions
    
    * docs: add codecov and build badges

--- lib/country_flags.dart
+++ lib/country_flags.dart
@@ -1,3 +1,6 @@
+/// A library to generate country flag widgets.
+///
+/// The flags can be genereated based on a language code or a country code.
 library country_flags;
 
 export 'src/country_flags.dart';
